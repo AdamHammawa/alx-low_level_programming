09fa27d3471b82b6c17a5cb0c60600123099c10f
AdamHammawa <adamsani2020@gmail.com>
2022-04-12 14:51:09 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 90);
+}
