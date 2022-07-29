83844679732bb6642996747a36cb30f370f6ea64
AdamHammawa <adamsani2020@gmail.com>
2022-07-29 16:20:34 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
+
 /**
- * key_index - function that gives you the index of a key.
+ * hash_table_set - sets a hash table with nodes
+ * @key: key to store the node
+ * @value: value of the node
+ * @ht: hash table
  *
- * @ht: The hash table you want to add or update the key/value to
- * @key: pointer to the key
- * @value: pointer to the value
- * Returns: 1 or 0
+ * Return: 1 if sucsess 0 if failed
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    
+	unsigned long int index = 0;
+	hash_node_t *check = NULL;
+	hash_node_t *new = NULL;
+
+
+	if (!ht || !key || !(*key) || !strcmp(key, "") || !value)
+		return (0); /* if null hash table, key, or empty key, error */
+
+	index = key_index((unsigned char *)key, ht->size);
+	check = ht->array[index];
+
+	while (check) /* check if the key already exists, if so replace. */
+	{
+		if (!strcmp(key, check->key)) /* strcmp returns 0 if matches */
+		{
+			free(check->value); /* get rid of old value, replace it */
+			check->value = strdup(value); /* str dup reallocs */
+			if (check->value)
+				return (1);
+			return (0);
+		}
+		check = check->next;
+	}
+
+	new = malloc(sizeof(hash_node_t)); /* malloc a node */
+	if (!new) /* malloc error */
+		return (0);
+
+	new->key = strdup(key); /* dup key */
+	new->value = strdup(value); /* dup value */
+
+	new->next = ht->array[index]; /* make the new node be the first */
+	ht->array[index] = new;
+	return (1);
 }
